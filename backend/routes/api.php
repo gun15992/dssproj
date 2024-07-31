edc09d4b636ce974d705a9acbe266952d86fe592
gun15992 <gun15992@gmail.com>
2024-07-31 09:32:53 +0700

    V0.0.1 - Beta Recovery Version
    Recovery file in DSS Inventory Web Application

---
+++ backend/routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LogController;
+use App\Http\Controllers\AuthController;
+
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getAuthenticatedUser']);
+
+Route::middleware('auth:sanctum')->get('users', function (Request $request) {
+    return $request->user();
+});
+
+// Route::resource('users', UserController::class);
+
+// Route::resource('products', ProductController::class);
+
+// Route::get('categories', [CategoryController::class, 'index']);
+
+// Route::resource('logs', LogController::class);
+// Route::delete('logs', [LogController::class, 'clearAllLogs']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('logs', LogController::class);
+    Route::delete('logs', [LogController::class, 'clearAllLogs']);
+});
+
+Route::get('categories', [CategoryController::class, 'index']);
