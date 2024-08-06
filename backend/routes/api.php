69409e51e7f3cb35b6ff552ab93ba30cb8769c86
gun15992 <gun15992@gmail.com>
2024-08-06 13:03:48 +0700

    V0.0.3 - Edit Route API (Edit)
    Edit api.php in backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,8 +13,6 @@ use App\Http\Controllers\AuthController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-// Route::middleware('auth:sanctum')->get('user', [UserController::class, 'getAuthenticatedUser']);
-
 Route::middleware('auth:sanctum')->get('users', function (Request $request) {
     return $request->user();
 });
