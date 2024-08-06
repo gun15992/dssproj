b4e154debba92c61ab642c58c532b60f3c999856
gun15992 <gun15992@gmail.com>
2024-08-06 12:44:52 +0700

    V0.0.3 - Edit Config (Edit)
    Edit config in backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,20 +13,14 @@ use App\Http\Controllers\AuthController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('user', [UserController::class, 'getAuthenticatedUser']);
+// Route::middleware('auth:sanctum')->get('user', [UserController::class, 'getAuthenticatedUser']);
 
 Route::middleware('auth:sanctum')->get('users', function (Request $request) {
     return $request->user();
 });
 
-// Route::resource('users', UserController::class);
-
-// Route::resource('products', ProductController::class);
-
-// Route::resource('logs', LogController::class);
-// Route::delete('logs', [LogController::class, 'clearAllLogs']);
-
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'getAuthenticatedUser']);
     Route::resource('users', UserController::class);
 
     Route::resource('products', ProductController::class);
