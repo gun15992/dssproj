78da4e9d5961e42e98b96ff8f513d07f8204514f
gun15992 <gun15992@gmail.com>
2024-08-06 14:10:01 +0700

    V0.0.3 - Change Title (Edit)
    Change title in backend

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DSS Inventory API</title>
+        <title>DSS Backend API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-        {{-- Icon --}}
+        <!-- Icon -->
         <link rel="icon" href="{{ asset('logo.png') }}" type="image/x-icon">
 
         <!-- Styles -->
--- backend/storage/framework/views/a6360aa601e243a8cd484eb40ec2c300.php
+++ backend/storage/framework/views/a6360aa601e243a8cd484eb40ec2c300.php
@@ -4,11 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DSS Inventory API</title>
+        <title>DSS Backend API</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
+        
         <link rel="icon" href="<?php echo e(asset('logo.png')); ?>" type="image/x-icon">
 
         <!-- Styles -->
